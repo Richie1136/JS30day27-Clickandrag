9ec07aef36e17df3f78a76ff496ce4d7a4689c3e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 13:22:26 -0400

    Adding the active class when i mouse down and removing that class when i mouse leave and mouse up

--- main.js
+++ main.js
@@ -6,17 +6,25 @@ let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
-
+  isDown = true
+  slider.classList.add('active')
+  console.log(e)
 })
 
 slider.addEventListener('mouseleave', (e) => {
+  isDown = false
+  slider.classList.remove('active')
 
 })
 
 slider.addEventListener('mouseup', (e) => {
+  isDown = false
+  slider.classList.remove('active')
 
 })
 
 slider.addEventListener('mousemove', (e) => {
-
+  if (!isDown) return
+  console.log(isDown)
+  console.log("Do Work")
 })
