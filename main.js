d89193ac905488631740a35c524cec192eab5f55
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 13:43:41 -0400

    Created 4 different functions for the mouse events and then added the functions as 2nd arguments

--- main.js
+++ main.js
@@ -5,29 +5,33 @@ let isDown = false
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true
-  slider.classList.add('active')
-  startX = e.pageX - slider.offsetLeft
-  scrollLeft = slider.scrollLeft
-})
-
-slider.addEventListener('mouseleave', (e) => {
-  isDown = false
-  slider.classList.remove('active')
-
-})
-
-slider.addEventListener('mouseup', (e) => {
-  isDown = false
-  slider.classList.remove('active')
-
-})
-
-slider.addEventListener('mousemove', (e) => {
+function Move(e) {
   if (!isDown) return
   e.preventDefault()
   const x = e.pageX - slider.offsetLeft
   const walk = x - startX
   slider.scrollLeft = scrollLeft - walk
-})
+}
+
+function MouseDown(e) {
+  isDown = true
+  slider.classList.add('active')
+  startX = e.pageX - slider.offsetLeft
+  scrollLeft = slider.scrollLeft
+}
+
+function MouseLeave(e) {
+  isDown = false
+  slider.classList.remove('active')
+}
+
+function MouseUp(e) {
+  isDown = false
+  slider.classList.remove('active')
+}
+
+slider.addEventListener('mousemove', Move)
+slider.addEventListener('mousedown', MouseDown)
+slider.addEventListener('mouseleave', MouseLeave)
+slider.addEventListener('mouseup', MouseUp)
+
