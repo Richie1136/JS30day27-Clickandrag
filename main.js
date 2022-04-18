efaaf820decb024c188acfa6423f0ea2cd423bd1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 13:15:08 -0400

    Added 4 different mouse events that will be needed

---
+++ main.js
@@ -0,0 +1,22 @@
+const slider = document.querySelector('.items')
+
+let isDown = false
+
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+
+})
+
+slider.addEventListener('mouseleave', (e) => {
+
+})
+
+slider.addEventListener('mouseup', (e) => {
+
+})
+
+slider.addEventListener('mousemove', (e) => {
+
+})
